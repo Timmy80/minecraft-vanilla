784b4c81d9f1c4b3195f6343e382cf4629a5b7e0
Anthony <anthony.800@live.fr>
2018-04-05 22:54:31 +0200

    implement new mojang download URL because the amazon S3 repository will be closed soon

--- resources/downloadMinecraftServer.py
+++ resources/downloadMinecraftServer.py
@@ -12,32 +12,52 @@ parser.add_argument("-t", "--target", type=str, default="release", choices=['rel
 args=parser.parse_args()
 
 version=""
+versionURL=""
 with open("/tmp/version_manifest.json") as json_data:
   manifest=json.load(json_data)
 
   if args.version=="latest" :
     print "looking for the latest minecraft "+args.target
-    version=manifest[args.version][args.target]
-    print "version "+version+" is the latest minecraft "+args.target+"!"
+    tmpVersion=manifest[args.version][args.target]
+    print "version "+tmpVersion+" is the latest minecraft "+args.target+"!"
+    for wVersion in manifest["versions"] : 
+      if wVersion["id"]==tmpVersion :
+        version=tmpVersion
+        versionURL=wVersion["url"]
+        print wVersion["type"]+" "+version+" found!"
+        break
 
-  else :
+  else : 
     print "looking for version "+args.version+" in minecraft available versions"
     for wVersion in manifest["versions"] :
       if wVersion["id"]==args.version :
         version=args.version
-	print wVersion["type"]+" "+version+" found!"
+        versionURL=wVersion["url"]
+        print wVersion["type"]+" "+version+" found!"
         break
-
 
 print
 if not version :
   print "requested version not found!"
   sys.exit(1)
 
-print "Start downloading version: "+version
+print "Start downloading version metadata : "+versionURL
+try:
+  jsonDowloader=urllib.URLopener()
+  jsonDowloader.retrieve(versionURL, "/tmp/"+version+".json")
+except IOError as e:
+  print "I/O Error. Download aborted "+str(e)
+  sys.exit(2)
+
+jarURL=""
+with open("/tmp/"+version+".json") as json_data:
+  version_manifest=json.load(json_data)
+  jarURL=version_manifest["downloads"]["server"]["url"]
+
+print "Start downloading server: "+jarURL
 try:
   jarfile=urllib.URLopener()
-  jarfile.retrieve("https://s3.amazonaws.com/Minecraft.Download/versions/"+version+"/minecraft_server."+version+".jar", "/tmp/minecraft_server."+version+".jar")
+  jarfile.retrieve(jarURL, "/tmp/minecraft_server."+version+".jar")
 except IOError as e:
   print "I/O Error. Download aborted "+str(e)
   sys.exit(2)
